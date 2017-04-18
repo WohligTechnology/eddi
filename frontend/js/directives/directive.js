ddc0fd182049501e518802738b936a31765c9349
Ruchira Padave <padave.ruchira29@gmail.com>
2017-04-18 17:30:50 +0530

    added

--- frontend/js/directives/directive.js
+++ frontend/js/directives/directive.js
@@ -101,6 +101,43 @@ myApp.directive('img', function ($compile, $parse) {
             }
         };
     })
+//alpha only and numbers only//
 
 
-;
+firstapp.directive('aplhaOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                var transformedInput = text.replace(/[^a-zA-Z]/g, '');
+                if (transformedInput !== text) {
+                    ngModelCtrl.$setViewValue(transformedInput);
+                    ngModelCtrl.$render();
+                }
+                return transformedInput;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
+
+firstapp.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
